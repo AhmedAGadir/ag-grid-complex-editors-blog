7ec6583e3809182d8c79cda946fb6695e7083608
AhmedAGadir <ahmed.gadir@gmail.com>
2020-05-05 01:23:32 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-enterprise";
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+import SimpleEditor from './Components/SimpleEditor';
 
 function App() {
+  const [columnDefs, setColumnDefs] = useState([
+    { headerName: 'Athelete (agTextCellEditor)', field: "athlete", cellEditor: 'agTextCellEditor' },
+    { headerName: "Age (simpleEditor)", field: "age", cellEditor: 'simpleEditor' },
+    { headerName: "Country", field: "country" },
+    { headerName: "Year", field: "year" },
+    { headerName: "Date", field: "date" },
+    { headerName: "Sport", field: "sport" },
+    { headerName: "Gold", field: "gold" },
+    { headerName: "Silver", field: "silver" },
+    { headerName: "Bronze", field: "bronze" },
+    { headerName: "Total", field: "total" }
+  ]);
+
+  const [defaultColDef, setDefaultColDef] = useState({
+    editable: true,
+
+  });
+
+  const [frameworkComponents, setFrameworkComponents] = useState({
+    simpleEditor: SimpleEditor
+  })
+
+  const [rowData, setRowData] = useState(null);
+  const [gridApi, setGridApi] = useState(null);
+  const [columnApi, setColumnApi] = useState(null);
+
+  const onGridReady = params => {
+    setGridApi(params.api);
+    setColumnApi(params.columnApi);
+
+    fetch("https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinnersSmall.json")
+      .then(res => res.json())
+      .then(data => {
+        setRowData(data);
+      });
+
+    params.columnApi.autoSizeAllColumns();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100%", height: "100vh" }}>
+      <div
+        id="myGrid"
+        style={{ height: "100%", width: "100%" }}
+        className="ag-theme-alpine">
+        <AgGridReact
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          frameworkComponents={frameworkComponents}
+          rowData={rowData}
+          onGridReady={onGridReady}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/SimpleEditor.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class extends Component {
+    constructor(props) {
+        super(props);
+        this.eRef = React.createRef();
+        this.state = {
+            value: ''
+        };
+    }
+
+    inputHandler = e => {
+        this.setState({ value: e.target.value });
+    }
+
+    getValue = () => {
+        return this.state.value;
+    }
+
+    afterGuiAttached = () => {
+        this.setState({ value: this.props.value }, () => {
+            this.eRef.current.focus();
+            this.eRef.current.select();
+        });
+    }
+
+    render() {
+        return (
+            <input
+                className="ag-input-field-input ag-text-field-input"
+                ref={this.eRef}
+                onChange={this.inputHandler}
+                value={this.state.value}
+            />
+        )
+    }
+}
